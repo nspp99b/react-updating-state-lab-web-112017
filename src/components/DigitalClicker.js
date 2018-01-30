5a5204fc797e9decb35abdb1f2a04d74abea2e1a
nspp99b <chromepanthers@gmail.com>
2018-01-30 15:57:42 -0500

    defined components with setstate functionality

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,28 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  handleClicker = (e) => {
+    this.setState({
+      timesClicked: this.state.timesClicked + 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={this.handleClicker}>{this.state.timesClicked}</button>
+      </div>
+    )
+  }
+}
+
+export default DigitalClicker;
